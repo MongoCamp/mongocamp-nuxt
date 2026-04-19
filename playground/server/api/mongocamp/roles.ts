f3c74ef8cfc845d6e94b3e0289371976323e05ea
sfxcode <tom@sfxcode.com>
2026-04-19 12:49:52 +0200

    style: format code for consistency and readability

--- playground/server/api/mongocamp/roles.ts
+++ playground/server/api/mongocamp/roles.ts
@@ -1,6 +1,6 @@
-import {mongocampApi} from "#server/utils/mongocamp";
+import { mongocampApi } from '#server/utils/mongocamp'
 
 export default defineEventHandler((event) => {
-  const {adminApi} = mongocampApi(event)
+  const { adminApi } = mongocampApi(event)
   return adminApi.listRoles()
-});
+})
--- playground/server/utils/mongocamp.ts
+++ playground/server/utils/mongocamp.ts
@@ -1,6 +1,6 @@
-import type {H3Event} from "h3";
+import type { H3Event } from 'h3'
 
-export function mongocampApi(event: H3Event)  {
+export function mongocampApi(event: H3Event) {
   const url = useRuntimeConfig(event).public.mongocamp?.url
   const apiKey = useRuntimeConfig(event).mongocampApiKey
 
